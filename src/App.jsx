19e96ac680738d0ec15d571f6a18e65545b24461
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2022-03-04 19:02:39 -0500

    Added general container

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Box } from '@mui/material'
 
 const App = () => {
   return (
-    <div>Hello Platzi World</div>
+    <Box
+      sx={{
+        width: '80vw',
+        height: '500px',
+        borderRadius: '16px',
+        boxShadow: '0px 0px 10px grey',
+        margin: '0 auto',
+        marginTop: '50px',
+        backgroundColor: 'whitesmoke'
+      }}
+    >
+      Hello Platzi World  
+    </Box>
   )
 }
 
